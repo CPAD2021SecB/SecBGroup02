48cdb81592446367deb976a0a1db80149329a6d9
RizuHabib <rizu.habib@gmail.com>
2021-11-03 05:30:00 +0530

    initialized firebase for the project

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { createSwitchNavigator, createAppContainer } from "react-navigation"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+firebase.initializeApp({
+  apiKey:"", //process.env.REACT_APP_apiKey,
+  authDomain: "", // process.env.REACT_APP_authDomain,
+  databaseURL: "", // process.env.REACT_APP_databaseURL,
+  projectId: "", // process.env.REACT_APP_projectId,
+  storageBucket: "", // process.env.REACT_APP_storageBucket,
+  messagingSenderId:"", // process.env.REACT_APP_messagingSenderId,
+  appId: ""// process.env.REACT_APP_appId,
+});
 
 
 const AppNavigator = createAppContainer(AppSwitchNavigator)
